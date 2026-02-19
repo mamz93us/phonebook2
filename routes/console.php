b153c86fa0449a28cb3cb01e91c3a23278f65abd
mamz93us <mamz93@icloud.com>
2026-02-20 00:09:07 +0300

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -4,12 +4,8 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-// Existing inspire command
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-// Auto-run GDMS sync hourly
-Schedule::command('gdms:sync-contacts')->hourly();
-// or for testing, every five minutes:
-// Schedule::command('gdms:sync-contacts')->everyFiveMinutes();
+Schedule::command('gdms:sync-contacts')->everyFiveMinutes();
